b900ec5a863a73a01717d54a37c731e64111b489
yichunzhao <zhaoyichun@gmail.com>
2022-05-07 23:24:21 +0200

    Initial commit

---
+++ src/main/java/com/example/connecttomysql/ConnectToMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.connecttomysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectToMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectToMySqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/connecttomysql/ConnectToMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connecttomysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectToMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
